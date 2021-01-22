f1425200e990678f2bef33b54f54e1b2adecb587
rederpz <rederpz@live.com>
2021-01-22 17:06:27 -0600

    Added open directory button Repositioned buttons to be more like the pause screen

--- src/main/java/ml/rederpz/betterlocalserver/screen/CreateServerScreen.java
+++ src/main/java/ml/rederpz/betterlocalserver/screen/CreateServerScreen.java
@@ -16,6 +16,7 @@ import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Util;
 import net.minecraft.world.GameMode;
 
 /**
@@ -27,7 +28,6 @@ public final class CreateServerScreen extends Screen {
     private final Screen parent;
     private CheckboxWidget cheatsCheckbox, flightCheckbox, pvpCheckbox, offlineCheckbox;
     private GameMode gameMode = GameMode.NOT_SET;
-    private SliderWidget viewDistanceSlider;
     private TextFieldWidget motdTextField;
     private ButtonWidget gameModeButton;
     private int port = 25565;
@@ -41,24 +41,25 @@ public final class CreateServerScreen extends Screen {
     @SuppressWarnings("ConstantConditions")
     @Override
     protected void init() {
-        this.addButton(this.motdTextField = new TextFieldWidget(this.textRenderer, this.width / 2 - 155, 75, 300, 20, new TranslatableText("internalServer.motd")));
+        final int height = (this.height / 4) - 16;
+        this.addButton(this.motdTextField = new TextFieldWidget(this.textRenderer, this.width / 2 - 155, height + 8, 300, 20, new TranslatableText("internalServer.motd")));
         this.motdTextField.setText(this.client.getServer().getServerMotd());
         this.motdTextField.setEditable(false);
 
-        this.addButton(this.gameModeButton = new ButtonWidget(this.width / 2 - 155, 100, 150, 20, LiteralText.EMPTY, (buttonWidget) -> {
+        this.addButton(this.gameModeButton = new ButtonWidget(this.width / 2 - 155, height + 32, 150, 20, LiteralText.EMPTY, (buttonWidget) -> {
             final int current = this.gameMode.ordinal();
             this.gameMode = GameMode.values()[current + 1 >= GameMode.values().length ? 0 : current + 1];
             this.updateButtonNames();
         }));
 
-        this.addButton(this.cheatsCheckbox = new CheckboxWidget(this.width / 2 + 5, 100, 100, 20, new TranslatableText("internalServer.allowCheats"), this.client.getServer().getPlayerManager().areCheatsAllowed()));
-        this.addButton(this.flightCheckbox = new CheckboxWidget(this.width / 2 + 5, 125, 100, 20, new TranslatableText("internalServer.allowFlight"), this.client.getServer().isFlightEnabled()));
-        this.addButton(this.offlineCheckbox = new CheckboxWidget(this.width / 2 + 5, 150, 100, 20, new TranslatableText("internalServer.offlineMode"), !this.client.getServer().isOnlineMode()));
-        this.addButton(this.pvpCheckbox = new CheckboxWidget(this.width / 2 + 5, 175, 100, 20, new TranslatableText("internalServer.pvp"), this.client.getServer().isPvpEnabled()));
+        this.addButton(this.cheatsCheckbox = new CheckboxWidget(this.width / 2 + 5, height + 32, 100, 20, new TranslatableText("internalServer.allowCheats"), this.client.getServer().getPlayerManager().areCheatsAllowed()));
+        this.addButton(this.flightCheckbox = new CheckboxWidget(this.width / 2 + 5, height + 56, 100, 20, new TranslatableText("internalServer.allowFlight"), this.client.getServer().isFlightEnabled()));
+        this.addButton(this.offlineCheckbox = new CheckboxWidget(this.width / 2 + 5, height + 80, 100, 20, new TranslatableText("internalServer.offlineMode"), !this.client.getServer().isOnlineMode()));
+        this.addButton(this.pvpCheckbox = new CheckboxWidget(this.width / 2 + 5, height + 104, 100, 20, new TranslatableText("internalServer.pvp"), this.client.getServer().isPvpEnabled()));
 
         final int viewDistance = this.client.getServer().getPlayerManager().getViewDistance();
         final double viewDistancePercent = (viewDistance - 2) / 30D;
-        this.addButton(this.viewDistanceSlider = new SliderWidget(this.width / 2 - 155, 125, 150, 20, new TranslatableText("internalServer.viewDistance", viewDistance), viewDistancePercent) {
+        this.addButton(new SliderWidget(this.width / 2 - 155, height + 56, 150, 20, new TranslatableText("internalServer.viewDistance", viewDistance), viewDistancePercent) {
             @Override
             protected void updateMessage() {
                 final int viewDistance = (int) (2 + (30 * this.value));
@@ -72,8 +73,9 @@ public final class CreateServerScreen extends Screen {
             }
         });
 
-        this.addButton(new ButtonWidget(this.width / 2 + 5, this.height - 28, 150, 20, ScreenTexts.CANCEL, (buttonWidget) -> this.client.openScreen(this.parent)));
-        this.addButton(new ButtonWidget(this.width / 2 - 155, this.height - 28, 150, 20, new TranslatableText("internalServer.start"), (buttonWidget) -> {
+        this.addButton(new ButtonWidget(this.width / 2 - 155, height + 80, 150, 20, new TranslatableText("internalServer.openDirectory"), button -> Util.getOperatingSystem().open(this.client.getServer().getIconFile().getParentFile().toURI())));
+        this.addButton(new ButtonWidget(this.width / 2 + 5, height + 128, 150, 20, ScreenTexts.CANCEL, (buttonWidget) -> this.client.openScreen(this.parent)));
+        this.addButton(new ButtonWidget(this.width / 2 - 155, height + 128, 150, 20, new TranslatableText("internalServer.start"), (buttonWidget) -> {
             this.client.openScreen(null);
 
             final TranslatableText responseText;
@@ -104,8 +106,8 @@ public final class CreateServerScreen extends Screen {
     @Override
     public void render(final MatrixStack matrices, final int mouseX, final int mouseY, final float delta) {
         this.renderBackground(matrices);
-        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 50, 0xFFFFFFFF);
-        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 50, 0xFFFFFFFF);
+        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 40, 0xFFFFFFFF);
+        DrawableHelper.drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 40, 0xFFFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
     }
 
