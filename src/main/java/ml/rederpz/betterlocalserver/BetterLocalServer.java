8dc2a37239fa72f9126739608f6145a7792d009d
rederpz <rederpz@live.com>
2021-01-22 13:41:32 -0600

    Initial commit.

---
+++ src/main/java/ml/rederpz/betterlocalserver/BetterLocalServer.java
@@ -0,0 +1,20 @@
+package ml.rederpz.betterlocalserver;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author Rederpz on Jan 22, 2021
+ */
+public final class BetterLocalServer implements ClientModInitializer, ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
